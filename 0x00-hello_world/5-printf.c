6e7193092a3196af9acc603641558185700db916
kenyanthugger <edwardsila000@gmail.com>
2022-09-08 19:57:47 -0700

    hellp printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the phras with proper grammer,
+ * but the outcome is a piece of art
+ * Return: 0 if exited well
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art, \n");
+	return (0);
+}
