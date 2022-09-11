89676f2cfb46274abeea11164a31ee0457b5a661
kenyanthugger <edwardsila000@gmail.com>
2022-09-11 04:22:02 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combo
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+		putchar(c);
+		if (c != 57)
+			putchar(',');
+			putchar('');
+	putchar('\n');
+	return (0);
+}
