d122dcdf44efebde96d3408b1612ec490053d73e
kenyanthugger <edwardsila000@gmail.com>
2022-09-11 03:12:48 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all letters in lowercase 
+ * expect for letters 'e' and 'q'
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	putchar('\n');
+	return (0);
+}
