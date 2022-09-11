ee5242067313ee9206fe26435f1cdde6105e4254
kenyanthugger <edwardsila000@gmail.com>
2022-09-11 03:29:04 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints all single characters of base 10
+ * starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	for (n = '0'; n <= '10'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
