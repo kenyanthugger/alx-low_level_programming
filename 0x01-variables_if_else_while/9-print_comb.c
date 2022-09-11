6321897a92d4b7e0498569dc142873cb22f34ba5
kenyanthugger <edwardsila000@gmail.com>
2022-09-11 04:24:51 -0700

    donw

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,9 @@ int main(void)
 
 	for (c = 48; c <= 57; c++)
 		putchar(c);
-		if (c != 57)
-			putchar(',');
-			putchar('');
+	if (c != 57)
+		putchar(',');
+	putchar('');
 	putchar('\n');
 	return (0);
 }
