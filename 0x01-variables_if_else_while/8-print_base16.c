775f761b18a618fcc8404df73f9bb02b648bbaca
kenyanthugger <edwardsila000@gmail.com>
2022-09-11 04:15:52 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers of base 16
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
