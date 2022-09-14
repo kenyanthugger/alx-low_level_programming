6032b68ee8d02882257e94dc7b1d055eab828f48
kenyanthugger <edwardsila000@gmail.com>
2022-09-14 16:33:51 -0700

    print all numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
  * add - adds two integers a and b
+ * @a: int to be added to b
+ * @b: int to be added to a
  * Return: returns c
  */
 int add(int a, int b)
 {
 	int c;
+
 	c = a + b;
 	return (c);
 }
