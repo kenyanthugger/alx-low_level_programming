0fb8fd04eee344d392ea1f79207a85018a930234
kenyanthugger <edwardsila000@gmail.com>
2022-09-30 14:38:34 -0700

    0 putchar

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,8 +2,9 @@
   
 /** 
 ** _puts_recursion - prints a string, followed by a new line 
-** @s: string to print 
-**/ 
+** @s: string to print
+** Return: 0
+*/
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
