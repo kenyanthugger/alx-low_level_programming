7f8463b21163c9df453b2838f14f62219d1e9032
kenyanthugger <edwardsila000@gmail.com>
2022-10-07 05:50:09 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * create_array - array to print strings
+ * @size - number element array
+ * @c - char
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	/* define values with malloc */
+
+	buffer =(char *) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+	
+	while (position < size)
+	{
+		*(buffer + position) = c;
+		position++;
+	}
+
+	return (buffer);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+repo contains malloc.projects
