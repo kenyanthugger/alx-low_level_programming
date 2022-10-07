8cbba9dcd964e2d3f9358d50c3f00de6d55ecd8f
kenyanthugger <edwardsila000@gmail.com>
2022-10-07 06:08:49 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 /**
  * create_array - array to print strings
- * @size - number element array
- * @c - char
+ * @size: number element array
+ * @c: char
  * Return: pointer
  */
 
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	}
 	/* define values with malloc */
 
-	buffer =(char *) malloc(size * sizeof(c));
+	buffer = (char *) malloc(size * sizeof(c));
 
 	if (buffer == 0)
 	{
